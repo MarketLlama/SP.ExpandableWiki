dcbdbefb7d1c611bf9bbe2d062db4fa3164aeec8
Mark Williams <mark@Marks-MacBook-Pro.local>
2019-06-03 12:53:58 +0100

    First deployment

--- src/webparts/expandableWikiPart/components/ExpandableWikiPart.tsx
+++ src/webparts/expandableWikiPart/components/ExpandableWikiPart.tsx
@@ -1,46 +1,69 @@
 import * as React from 'react';
 import styles from './ExpandableWikiPart.module.scss';
+import posed from 'react-pose';
 import { IExpandableWikiPartProps } from './IExpandableWikiPartProps';
 import { IExpandableWikiPartState } from './IExpandableWikiPartState';
-import { escape } from '@microsoft/sp-lodash-subset';
 import { ActionButton } from 'office-ui-fabric-react';
 import { RichText } from "@pnp/spfx-controls-react/lib/RichText";
 import { DisplayMode } from '@microsoft/sp-core-library';
 
 
 export default class ExpandableWikiPart extends React.Component<IExpandableWikiPartProps, IExpandableWikiPartState> {
+  private Content = posed.div({
+    closed: { height: 0 },
+    open: { height: 'auto' }
+  });
+
   /**
    *
    */
   constructor(props : IExpandableWikiPartProps) {
     super(props);
     this.state ={
-      displayMode : false
+      displayMode : false,
+      isOpen : false
     };
     this._editModeFix();
+    let numberOfLines : number;
+    if(!props.numberOfLines){
+      numberOfLines = 3;
+    } else{
+      numberOfLines = props.numberOfLines;
+    }
+    this.Content = posed.div({
+      closed: { height: `${numberOfLines * 50}px` },
+      open: { height: 'auto' }
+    });
   }
 
   public render(): React.ReactElement<IExpandableWikiPartProps> {
+    const isOpen = this.state.isOpen || (this.props.displayMode == DisplayMode.Edit);
     return (
       <div className={ styles.expandableWikiPart }>
         <div className={ styles.container }>
-          <div>
-          <RichText value={this.props.text}
-            isEditMode={(this.props.displayMode == DisplayMode.Edit)}
-            onChange={(text)=>this._setText(text)}
-          />
-          </div>
+          <this.Content
+            pose={isOpen== true ? 'open' : 'closed'}
+            className={`${styles.textBox} ${(isOpen? styles.isOpen : null)}`}
+          >
+            <RichText value={this.props.text}
+              isEditMode={(this.props.displayMode == DisplayMode.Edit)}
+              onChange={(text)=>this._setText(text)}
+            />
+          </this.Content>
           <ActionButton
-            text="Expand"
-            iconProps={{ iconName: 'AddFriend' }}
+            text={isOpen == true? 'View less' : 'View more'}
+            iconProps={isOpen == true? { iconName: 'ChevronUp' } : { iconName: 'ChevronDown' }}
             onClick={this._expandField}
+            disabled={this.props.displayMode == DisplayMode.Edit}
           />
         </div>
       </div>
     );
   }
   private _expandField =() =>{
-    console.log('done');
+    this.setState({
+      isOpen : !this.state.isOpen
+    });
   }
 
   private _setText = (text: string): string => {
--- src/webparts/expandableWikiPart/components/IExpandableWikiPartState.ts
+++ src/webparts/expandableWikiPart/components/IExpandableWikiPartState.ts
@@ -1,3 +1,4 @@
 export interface IExpandableWikiPartState {
   displayMode : boolean;
+  isOpen : boolean;
 }
